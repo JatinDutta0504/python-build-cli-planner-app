454ad1c7fc8236df92c9fb8f8e8f17598569d483
Jatin Dutta <54654364+JatinDutta0504@users.noreply.github.com>
2020-12-25 20:48:18 -0800

    module 3

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+from collections.abc import Iterable
+
+class DeadlinedMetaReminder(Iterable,ABCMeta=metaclass):
+
+    @abstractmethod
+    def is_due():
+        pass
+
+    
